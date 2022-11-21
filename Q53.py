f9a0bc1fe3112092f7d952680d8bb4c562c2b3bc
amiloserny <116841799+amiloserny@users.noreply.github.com>
2022-11-21 14:20:57 -0500

    Add files via upload

---
+++ Q53.py
@@ -0,0 +1,8 @@
+#Amanda Miloserny
+import os
+os.chdir(r"C:\Users\amiloserny01\Desktop\Quiz 5")
+cwd = os.getcwd()
+cwd
+os.mkdir("Documents1")
+os.listdir()
+os.rename("text1.txt", "Documents1/text1.txt")
